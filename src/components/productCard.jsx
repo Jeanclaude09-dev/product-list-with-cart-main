e66bf420887fe787e36e1771dd26ab8313861afc
Jeanclaude09-dev <jeanclaudenkongolo7@gmail.com>
2026-04-11 14:38:41 +0300

    ft(dynamic button)

--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -1,8 +1,27 @@
 import { useApp } from "@/context/appContext";
-import { ShoppingCart } from "lucide-react";
+import { CircleMinus, CirclePlus, ShoppingCart } from "lucide-react";
+import { useState } from "react";
 
 function ProductCard() {
   const { data } = useApp();
+  const [cartItems, setCartItems] = useState({});
+
+
+  // prev is current state before the update
+  const addToCart = (id) => {
+    setCartItems(prev => ({ ...prev, [id]: (prev[id] || 0) + 1 }));
+  };
+
+  const decrement = (id) => {
+    setCartItems(prev => {
+      if (prev[id] <= 1) {
+        const updated = { ...prev };
+        delete updated[id];
+        return updated;
+      }
+      return { ...prev, [id]: prev[id] - 1 };
+    });
+  };
 
   return (
 
@@ -15,13 +34,14 @@ function ProductCard() {
       <div className="grid grid-cols-3 gap-5 group">
         {data.map((items) => (
           <div key={items.id}>
-            <div className="flex flex-col items-center group relative mb-8">
+            <div
+              className={`flex flex-col ${cartItems[items.id] ? "border-2 border-orange-500" : ""} rounded-2xl items-center group relative mb-8`}>
               <picture>
                 <source
                   media="(min-width: 1024px)"
                   srcset={items.image.desktop}
                   alt={items.image.alt}
-                  className="rounded-2xl "
+                  className="rounded-2xl"
                 />
                 <source
                   media="(min-width: 660px)"
@@ -35,10 +55,25 @@ function ProductCard() {
                   className="rounded-2xl"
                 />
               </picture>
-              <button className="flex items-center gap-2 absolute -bottom-5 px-4 py-2 bg-rose-50 rounded-full  border border-rose-500 cursor-pointer ">
-                <ShoppingCart color="#c73a0f" />
-                <span>Add to Cart</span>
-              </button>
+
+
+              {cartItems[items.id] ? (
+                <div className="flex items-center gap-2 absolute -bottom-5 px-4 py-2 bg-orange-500 rounded-full  cursor-pointer ">
+                  <button onClick={() => decrement(items.id)}><CircleMinus className="cursor-pointer" /></button>
+                  <span>{cartItems[items.id]}</span>
+                  <button onClick={() => addToCart(items.id)}><CirclePlus className="cursor-pointer" /></button>
+                </div>
+              ) :
+                (
+                  <button
+                    onClick={() => addToCart(items.id)}
+                    className="flex items-center gap-2 absolute -bottom-5 px-4 py-2 bg-rose-50 rounded-full  border border-rose-500 cursor-pointer ">
+                    <ShoppingCart color="#c73a0f" />
+                    <span>Add to Cart</span>
+                  </button>
+                )}
+
+
             </div>
             <p className="text-rose-300">{items.category}</p>
             <h2 className="text-rose-900 text-lg">{items.name}</h2>
